9783944d6d023e38b1a92658eb87ee83517678b0
JohnsonLiu <379937065@qq.com>
2018-09-11 14:06:39 +0800

    vesion1.0

---
+++ gulp-project/gulpfile.js
@@ -0,0 +1,51 @@
+
+let gulp = require('gulp');
+let minifyJS = require('gulp-babel-minify');
+let minifyCSS = require('gulp-clean-css');
+let connect = require('gulp-connect');
+
+gulp.task('default', function () {
+    console.log('default task');
+});
+
+gulp.task('build', function () {
+    gulp.src('./src/**/*.html')
+        .pipe(gulp.dest('./dist'));
+    gulp.src('./src/**/*.css')
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('./dist'));
+    gulp.src('./src/**/*.js')
+        .pipe(minifyJS())
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('refreshHTML', function () {
+    gulp.src('./src/**/*.html')
+        .pipe(gulp.dest('./dist'))
+        .pipe(connect.reload());
+});
+
+gulp.task('refreshCSS', function () {
+    gulp.src('./src/**/*.css')
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('refreshJS', function () {
+    gulp.src('./src/**/*.js')
+        .pipe(minifyJS())
+        .pipe(gulp.dest('./dist'));
+})
+
+gulp.task('server', function () {
+    connect.server({
+        name: '',
+        root: ['dist'],
+        port: 9999,
+        livereload: true
+    });
+    gulp.watch(['./src/**/*.html'], ['refreshHTML']);
+    gulp.watch(['./src/**/*.css'], ['refreshCSS', 'refreshHTML']);
+    gulp.watch(['./src/**/*.js'], ['refreshJS', 'refreshHTML']);
+
+})
